0153dd3800549cbfdaa116b13e265d1a908a9a72
erik.kappelman <erik@duplovici.com>
2015-08-21 20:19:09 -0600

    M

--- date-compare.js
+++ date-compare.js
@@ -2,6 +2,8 @@ exports.dateCompare = function(dateToCheck, howManyDaysAgo) {
   var today = new Date();
   var dynoDate = new Date();
   dynoDate.setDate(dynoDate.getDate() - howManyDaysAgo);
+  console.log(dynoDate);
+  console.log(dateToCheck);
   if (dynoDate >= dateToCheck) {
     console.log("BEFORE");
     return true;
