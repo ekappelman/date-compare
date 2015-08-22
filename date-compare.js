5b7b28179560d9301f3823fd437e4584356228d0
erik.kappelman <erik@duplovici.com>
2015-08-21 20:46:22 -0600

    Fully functional

--- date-compare.js
+++ date-compare.js
@@ -4,13 +4,9 @@ exports.dateCompare = function(dateToCheck, howManyDaysAgo) {
   dynoDate.setDate(dynoDate.getDate() - howManyDaysAgo);
   var checkDate = Date.parse(dynoDate);
   var inDate = Date.parse(dateToCheck);
-  console.log(checkDate);
-  console.log(inDate);
   if (checkDate >= inDate) {
-    console.log("BEFORE");
     return true;
   } else {
-    console.log("AFTER");
     return false;
   }
 }
